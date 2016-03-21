00697dcca0724cccf36177eb3b94277566279187
Angel Vladov <angel.vladov@d-consult.net>
2016-03-21 12:16:08 +0200

    Horizontal and vertical direction

--- src/emlastic.js
+++ src/emlastic.js
@@ -1,15 +1,87 @@
 /*
  *
- *
- *
  * Copyright (c) 2016 Angel Vladov
  * Licensed under the MIT license.
  */
 (function ($) {
 	$.fn.emlastic = function () {
-		return this.each(function (i) {
-			// Do something to each selected element.
-			$(this).html('emlastic' + i);
+		return this.each(function () {
+			var $element = $(this);
+			var containerElement = null;
+			var previousValue = 0;
+			var totalEms = parseInt(opts.designSize) / parseFloat(opts.pixelsInEm);
+
+			if (!totalEms || isNaN(totalEms)) {
+				totalEms = 100; // TODO: Throw an error
+			}
+
+			switch (opts.container) {
+				case 'window':
+					containerElement = window;
+					break;
+
+				default:
+					containerElement = this;
+					break;
+			}
+
+			function calculateContainerSize() {
+				if (opts.containerSize != null) { // jshint ignore:line
+					if ($.isNumeric(opts.containerSize)) {
+						return parseInt(opts.containerSize);
+					} else if ($.isFunction(opts.containerSize)) {
+						return opts.containerSize($element[0], opts);
+					} else {
+						throw 'Container size can only be number or function.';
+					}
+				}
+
+				switch (opts.direction) {
+					case 'horizontal':
+						return containerElement.innerHeight;
+
+					case 'vertical':
+						return containerElement.innerWidth;
+
+					default:
+						return containerElement.innerWidth;
+				}
+			}
+
+			function onResize() {
+				var containerSize = calculateContainerSize();
+
+				if (containerSize) {
+					containerSize = parseFloat(containerSize);
+
+					var newValue = (containerSize / totalEms * 6.25);
+
+					if (!previousValue || newValue != previousValue) {
+						// Use pixelsInEm of 100 whenever possible. Android doesn't allow em size less than 8px
+						$element.css('fontSize', newValue + '%');
+
+						previousValue = newValue;
+					}
+				}
+
+				$element.removeClass('emlastic-running');
+			}
+
+			$element.addClass('emlastic-running');
+
+			setTimeout(onResize, 0);
+
+			$(window)
+				.on('resize', onResize)
+				.on('orientationchange', onResize);
 		});
 	};
+
+	$.fn.emlastic.defaults = {
+		pixelsInEm: 100,
+		designSize: 1920,
+		direction: 'horizontal',
+		containerSize: null,
+		container: null // By default this is the element where you are attaching the directive
+	}
 }(jQuery));
